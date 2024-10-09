28d19094f2f0bf5b3e47733ea8dd32fed0bf0970
Ayush <iamayush02@gmail.com>
2024-10-09 21:01:28 +0530

    Homepage implementation completed

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,14 +1,180 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
+import randomImage from '../assets/images/randomImage';
+
+const items = [
+  {
+    id: 1,
+    place: 'Noida',
+    state: 'UP',
+  },
+  {
+    id: 2,
+    place: 'Patna',
+    state: 'Bihar',
+  },
+  {
+    id: 3,
+    place: 'Kochi',
+    state: 'Kerala',
+  },
+  {
+    id: 4,
+    place: 'Delhi',
+    state: 'New Delhi',
+  },
+];
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      {/* header */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Expensify</Text>
+        <TouchableOpacity style={styles.logBtn}>
+          <Text style={styles.logBtnText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+      {/* image container */}
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/images/banner.png')}
+          style={styles.image}
+        />
+      </View>
+      {/* recent trip header */}
+      <View style={styles.tripHeader}>
+        <Text style={styles.tripText}>Recent Trips</Text>
+        <TouchableOpacity style={styles.addTrip}>
+          <Text style={styles.addTripText}>Add Trip</Text>
+        </TouchableOpacity>
+      </View>
 
-export default HomeScreen
+      {/* Flatlist */}
+      <FlatList
+        data={items}
+        numColumns={2}
+        keyExtractor={item => item.id}
+        showsVerticalScrollIndicator={false}
+        columnWrapperStyle={{
+          justifyContent: 'space-between',
+        }}
+        renderItem={({item}) => {
+          return (
+            <TouchableOpacity style={styles.card}>
+              <View>
+                <Image
+                  source={randomImage()}
+                  style={styles.cardImage}
+                />
+                <Text style={styles.placeText}>{item.place}</Text>
+                <Text style={styles.stateText}>{item.state}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 40,
+    fontWeight: '700',
+    color: '#000',
+    padding: 5,
+  },
+  logBtn: {
+    width: 70,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 5,
+    backgroundColor: 'dodgerblue',
+  },
+  logBtnText: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#000',
+  },
+  imageContainer: {
+    height: 300,
+    backgroundColor: 'lightblue',
+    width: '95%',
+    borderRadius: 20,
+    margin: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  image: {
+    height: 300,
+    width: 300,
+  },
+  tripHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 5,
+  },
+  tripText: {
+    fontSize: 24,
+    fontWeight: '800',
+    color: '#000',
+  },
+  addTrip: {
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 20,
+    color: '#fff',
+  },
+  addTripText: {
+    fontSize: 14,
+    color: '#000',
+    fontWeight: '600',
+  },
+  cardImage: {
+    height: 190,
+    width: 190,
+    // margin:5
+  },
+  placeText: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '800',
+    color: '#000',
+  },
+  stateText: {
+    textAlign: 'center',
+    fontWeight:'600',
+    marginBottom:10,
+    fontSize:14,
+    color:'grey'
+  },
+  card: {
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOpacity: 1,
+    margin: 5,
+    backgroundColor:'#fff'
+  },
+});
