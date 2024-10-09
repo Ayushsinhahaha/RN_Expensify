c55c73a4689c517fb293d8bbda7879d49e677356
Ayush <iamayush02@gmail.com>
2024-10-09 18:16:07 +0530

    Stack Navigation Implementation completed

---
+++ client/navigation/appNavigation.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+
+const Stack=createNativeStackNavigator();
+
+const appNavigation = () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="HomeScreen" >
+            <Stack.Screen name="HomeScreen" component={HomeScreen}  options={{headerShown:false}} />
+            <Stack.Screen name="LoginScreen" component={LoginScreen}  options={{headerShown:false}} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default appNavigation
+
+const styles = StyleSheet.create({})
